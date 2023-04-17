4e979660bfa35ad8ae581f6296b6d030cb620c19
twinstp <twinstp@protonmail.com>
2023-04-17 15:50:42 -0400

    mo

--- content.js
+++ content.js
@@ -6,11 +6,7 @@
     MAX_CACHE_SIZE: 500,
     MAX_HAMMING_DISTANCE: 3,
     FILTERED_SUBSTRINGS: [
-      "modification, and he recently agreed to answer our questions",
-      "legal efforts to overturn the 2020 election; and three offenses relating to Trump’s unlawful possession of government records at Mar-a-Lago",
-      "America is in the midst of the Cold War. The masculine fire and fury of World War II has given way to a period of cooling",
-      "Go to the link, and look at that woman. Look at that face. She never expressed any remorse over",
-      "destroyed the Ancien Regime in Europe, was an economic and scientific golden era, but politically it was a mess."
+      //...
     ]
   };
 
@@ -35,19 +31,8 @@
       id: table.id
     }));
 
-    // Load crypto-js.min.js and filterWorker.js as strings (locally)
-    const cryptoJsScript = await (await fetch(chrome.runtime.getURL('crypto-js.min.js'))).text();
-    const filterWorkerScript = await (await fetch(chrome.runtime.getURL('filterWorker.js'))).text();
-
-    // Combine the two scripts into one
-    const combinedScript = `${cryptoJsScript}\n${filterWorkerScript}`;
-
-    // Create a Blob URL for the combined script
-    const blob = new Blob([combinedScript], { type: 'text/javascript' });
-    const blobURL = URL.createObjectURL(blob);
-
-    // Create a Web Worker using the Blob URL
-    const filterWorker = new Worker(blobURL);
+    // Create a Web Worker from the filterWorker.js file
+    const filterWorker = new Worker(chrome.runtime.getURL('filterWorker.js'));
 
     // Send data to the worker, including postData, config, and filteredSubstrings (converted to array)
     filterWorker.postMessage({
@@ -83,29 +68,29 @@
     chrome.storage.local.set({ config }, () => {
       console.log(`Added user-defined substring "${substring}" to the filter list.`);
     });
-    }
-    
-    // Register a listener for the "addUserFilteredSubstring" message
-    // This allows the background or options page to request the addition of a new substring to the filter
-    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  }
+
+  // Register a listener for the "addUserFilteredSubstring" message
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.type === 'addUserFilteredSubstring' && message.substring) {
-    addUserFilteredSubstring(message.substring);
-    sendResponse({ success: true });
+      addUserFilteredSubstring(message.substring);
+      sendResponse({ success: true });
     }
-    });
-    
-    // Invoke the filterSpamPosts function again to apply changes if the configuration is updated
-    chrome.storage.onChanged.addListener((changes, areaName) => {
+    return false; // Indicate that the listener will not respond asynchronously
+  });
+
+  // Invoke the filterSpamPosts function again to apply changes if the configuration is updated
+  chrome.storage.onChanged.addListener((changes, areaName) => {
     if (areaName === 'local' && changes.config) {
-    // Update the local configuration and filtered substrings
-    config = changes.config.newValue;
-    filteredSubstrings.clear();
-    config.FILTERED_SUBSTRINGS.forEach((substring) => filteredSubstrings.add(substring));
-          // Re-run the filter
+      // Update the local configuration and filtered substrings
+      config = changes.config.newValue;
+      filteredSubstrings.clear();
+      config.FILTERED_SUBSTRINGS.forEach((substring) => filteredSubstrings.add(substring));
+      // Re-run the filter
       filterSpamPosts();
     }
-    });
-    
-    })().catch((error) => {
-    console.error('Error in extension:', error.message);
-    });
+  });
+
+})().catch((error) => {
+  console.error('Error in extension:', error.message);
+});
