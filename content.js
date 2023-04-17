36c59e81c9951a547fe169c1331e41e8777ff2c7
twinstp <twinstp@protonmail.com>
2023-04-17 13:07:49 -0400

    blob6

--- content.js
+++ content.js
@@ -16,37 +16,44 @@
       id: table.id
     }));
 
-    // Load crypto-js.min.js and filterWorker.js as strings
-    const cryptoJsScript = await fetch(chrome.runtime.getURL('crypto-js.min.js')).then((res) => res.text());
+    // Load filterWorker.js as a string
     const filterWorkerScript = await fetch(chrome.runtime.getURL('filterWorker.js')).then((res) => res.text());
 
-    // Combine the two scripts into one
-    const combinedScript = `${cryptoJsScript}\n${filterWorkerScript}`;
-
-    // Create a Blob URL for the combined script
-    const blob = new Blob([combinedScript], { type: 'text/javascript' });
+    // Create a Blob URL for the worker script
+    const blob = new Blob([filterWorkerScript], { type: 'text/javascript' });
     const blobURL = URL.createObjectURL(blob);
 
-// Create a Web Worker using the Blob URL
-const filterWorker = new Worker(blobURL);
+    // Create a Web Worker using the Blob URL
+    const filterWorker = new Worker(blobURL);
 
-// Send data to the worker, including postData and filteredSubstrings (converted to array)
-filterWorker.postMessage({
-  postData,
-  config,
-  filteredSubstrings: [...filteredSubstrings] // Convert the set to an array
-});
+    // Send data to the worker, including postData and filteredSubstrings (converted to array)
+    filterWorker.postMessage({
+      postData,
+      config,
+      filteredSubstrings: [...filteredSubstrings] // Convert the set to an array
+    });
 
-// Receive filtered data from the worker
-filterWorker.onmessage = (event) => {
-  const { filteredIds } = event.data;
-  filteredIds.forEach((id) => {
-    const table = document.getElementById(id);
-    table.style.visibility = 'hidden';
-    table.style.display = 'none';
-  });
-};
+    // Receive filtered data from the worker
+    filterWorker.onmessage = (event) => {
+      const { filteredIds } = event.data;
+      filteredIds.forEach((id) => {
+        const table = document.getElementById(id);
+        // Check if the element with the given ID exists before modifying its style
+        if (table) {
+          table.style.visibility = 'hidden';
+          table.style.display = 'none';
+        } else {
+          // Optionally, log a message if the element is not found
+          console.warn(`Element with ID ${id} not found.`);
+        }
+      });
 
+      // Terminate the worker
+      filterWorker.terminate();
+    };
+
+    // Revoke the Blob URL to free memory
+    URL.revokeObjectURL(blobURL);
   }
 
   // Invoke the filterSpamPosts function to start filtering
