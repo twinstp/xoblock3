ebb1c6c5a94081b0ec5118fed385fe0ec8c06166
twinstp <twinstp@protonmail.com>
2023-04-17 12:09:05 -0400

    fix worker cross-origin issue w blob-pt2

--- content.js
+++ content.js
@@ -5,19 +5,6 @@
   // Load the crypto-js library from the extension's local directory
   const cryptoJsScript = document.createElement('script');
   cryptoJsScript.src = chrome.runtime.getURL('crypto-js.min.js');
-
-  // Log when the script has loaded successfully
-  cryptoJsScript.onload = () => {
-    console.log('crypto-js script loaded successfully');
-    // Invoke the filterSpamPosts function to start filtering after script loads
-    filterSpamPosts();
-  };
-
-  // Log and handle errors if the script fails to load
-  cryptoJsScript.onerror = (error) => {
-    console.error('Failed to load crypto-js script:', error);
-  };
-
   document.head.appendChild(cryptoJsScript);
 
   // Load the configuration from config.json
@@ -29,28 +16,20 @@
 
   // Function to filter out spam posts from the current page view
   async function filterSpamPosts() {
-    // Check if the crypto-js script has been successfully loaded
-    if (typeof CryptoJS === 'undefined') {
-      console.error('CryptoJS library not loaded. Cannot proceed with filtering.');
-      return;
-    }
-
     const messageTables = document.querySelectorAll("table[width='700']");
     const postData = Array.from(messageTables).map(table => ({
       content: table.innerText.trim(), // Extract and trim text content
       id: table.id // Identify the table element
     }));
 
-    // Fetch worker script, convert to Blob, and create Blob URL
-    const workerScript = await fetch(chrome.runtime.getURL('filterWorker.js')).then((response) => response.text());
-    const workerBlob = new Blob([workerScript], { type: 'text/javascript' });
-    const workerBlobUrl = URL.createObjectURL(workerBlob);
+    // Create a Web Worker for filtering
+    const filterWorker = new Worker('filterWorker.js');
 
-    // Create a Web Worker for filtering using the Blob URL
-    const filterWorker = new Worker(workerBlobUrl);
+    // Get the URL for crypto-js.min.js
+    const cryptoJsUrl = chrome.runtime.getURL('crypto-js.min.js');
 
-    // Send data to the worker, including postData and filteredSubstrings
-    filterWorker.postMessage({ postData, config, filteredSubstrings });
+    // Send data to the worker, including postData, filteredSubstrings, and cryptoJsUrl
+    filterWorker.postMessage({ postData, config, filteredSubstrings, cryptoJsUrl });
 
     // Receive filtered data from the worker
     filterWorker.onmessage = (event) => {
@@ -61,20 +40,13 @@
         table.style.display = 'none';
       });
     };
-
-    // Log error if the worker encounters an error
-    filterWorker.onerror = (error) => {
-      console.error('Filter worker encountered an error:', error);
-    };
   }
+
+  // Invoke the filterSpamPosts function to start filtering
+  await filterSpamPosts();
 
   // Function to allow users to update userFilteredSubstrings
   function addUserFilteredSubstring(substring) {
     filteredSubstrings.add(substring);
-    console.log(`Added user-defined substring to filter: "${substring}"`);
   }
-
-  // Test: Adding a user-defined substring to filter
-addUserFilteredSubstring('test substring');
-console.log('Current filtered substrings:', Array.from(filteredSubstrings));
 })();
--- filterWorker.js
+++ filterWorker.js
@@ -1,103 +1,106 @@
 // filterWorker.js
-self.importScripts(chrome.runtime.getURL('crypto-js.min.js')); // Use chrome.runtime.getURL to get the correct URL
-
-// Utility function to tokenize a message
-function tokenize(message) {
-  return message.split(/\s+/);
-}
-
-// Utility function to compute SimHash of a message
-function computeSimHash(message) {
-  if (typeof BigInt === 'undefined') {
-    // BigInt is not supported, handle accordingly
-    return null;
-  }
-
-  const tokenWeights = new Map();
-  const tokens = tokenize(message);
-  tokens.forEach((token) => {
-    const weight = tokenWeights.get(token) || 0;
-    tokenWeights.set(token, weight + 1);
-  });
-
-  const fingerprintBits = 64;
-  const v = new Int32Array(fingerprintBits);
-  for (const [token, weight] of tokenWeights.entries()) {
-    const hash = BigInt('0x' + CryptoJS.SHA1(token).toString());
-    for (let i = 0; i < fingerprintBits; ++i) {
-      v[i] += (hash >> BigInt(i)) & BigInt(1) ? weight : -weight;
-    }
-  }
-
-  let simHash = BigInt(0);
-  for (let i = 0; i < fingerprintBits; ++i) {
-    if (v[i] > 0) {
-      simHash |= BigInt(1) << BigInt(i);
-    }
-  }
-  return simHash;
-}
-
-// Utility function to calculate Hamming distance between two SimHashes
-function hammingDistance(hash1, hash2) {
-  if (typeof BigInt === 'undefined') {
-    // BigInt is not supported, handle accordingly
-    return null;
-  }
-
-  const x = hash1 ^ hash2;
-  return x.toString(2).split('').filter((bit) => bit === '1').length;
-}
-
-// Initialize messageCache and cacheIndex
-const messageCache = [];
-let cacheIndex = 0;
-
 self.onmessage = (event) => {
-  const { postData, config, filteredSubstrings } = event.data;
-  const filteredIds = [];
-  const MAX_CACHE_SIZE = config.MAX_CACHE_SIZE || 500; // Set default value if not provided
+    const { postData, config, filteredSubstrings, cryptoJsUrl } = event.data;
   
-  // Filter logic (similar to the original filterSpamPosts function)
-  postData.forEach((post) => {
-    const { content, id } = post;
-    const joinedString = content.trim();
-
-    // Ensure that only messages with more than 250 printable characters are filtered out
-    if (joinedString.length <= 250) {
-      return;
+    // Load the crypto-js library in the worker using the provided URL
+    self.importScripts(cryptoJsUrl);
+  
+    // Utility function to tokenize a message
+    function tokenize(message) {
+      return message.split(/\s+/);
     }
-
-    // Check if the post content matches any predefined substrings
-    if (filteredSubstrings.some((substring) => joinedString.includes(substring))) {
-      filteredIds.push(id); // Store the ID of the filtered post
-      return; // Exit early from the loop
+  
+    // Utility function to compute SimHash of a message
+    function computeSimHash(message) {
+      if (typeof BigInt === 'undefined') {
+        // BigInt is not supported, handle accordingly
+        return null;
+      }
+  
+      const tokenWeights = new Map();
+      const tokens = tokenize(message);
+      tokens.forEach((token) => {
+        const weight = tokenWeights.get(token) || 0;
+        tokenWeights.set(token, weight + 1);
+      });
+  
+      const fingerprintBits = 64;
+      const v = new Int32Array(fingerprintBits);
+      for (const [token, weight] of tokenWeights.entries()) {
+        const hash = BigInt('0x' + CryptoJS.SHA1(token).toString());
+        for (let i = 0; i < fingerprintBits; ++i) {
+          v[i] += (hash >> BigInt(i)) & BigInt(1) ? weight : -weight;
+        }
+      }
+  
+      let simHash = BigInt(0);
+      for (let i = 0; i < fingerprintBits; ++i) {
+        if (v[i] > 0) {
+          simHash |= BigInt(1) << BigInt(i);
+        }
+      }
+      return simHash;
     }
-
-    // Compute the SimHash of the post content
-    const simHash = computeSimHash(joinedString);
-    if (!simHash) {
-      // If simHash is null (BigInt not supported), skip this iteration
-      return;
+  
+    // Utility function to calculate Hamming distance between two SimHashes
+    function hammingDistance(hash1, hash2) {
+      if (typeof BigInt === 'undefined') {
+        // BigInt is not supported, handle accordingly
+        return null;
+      }
+  
+      const x = hash1 ^ hash2;
+      return x.toString(2).split('').filter((bit) => bit === '1').length;
     }
+  
+    // Initialize messageCache and cacheIndex
+    const messageCache = [];
+    let cacheIndex = 0;
+    const MAX_CACHE_SIZE = config.MAX_CACHE_SIZE || 500; // Set default value if not provided
+    const filteredIds = [];
+  
+    // Filter logic (similar to the original filterSpamPosts function)
+    postData.forEach((post) => {
+      const { content, id } = post;
+      const joinedString = content.trim();
+  
+      // Ensure that only messages with more than 250 printable characters are filtered out
+      if (joinedString.length <= 250) {
+        return;
+      }
+  
+      // Check if the post content matches any predefined substrings
+      if (filteredSubstrings.some((substring) => joinedString.includes(substring))) {
+        filteredIds.push(id); // Store the ID of the filtered post
+        return; // Exit early from the loop
+      }
+  
+      // Compute the SimHash of the post content
+      const simHash = computeSimHash(joinedString);
+      if (!simHash) {
+        // If simHash is null (BigInt not supported), skip this iteration
+        return;
+      }
+  
+      // Check if the SimHash is similar to any cached SimHashes based on a threshold MAX_HAMMING_DISTANCE
+      const isSpamBySimHash = messageCache.some(
+        (cachedHash) => hammingDistance(simHash, cachedHash) <= config.MAX_HAMMING_DISTANCE
+      );
+      if (isSpamBySimHash) {
+        filteredIds.push(id); // Store the ID of the filtered post
+      } else
+      {
+        // Update messageCache with the new SimHash
+        messageCache[cacheIndex] = simHash;
+        cacheIndex = (cacheIndex + 1) % MAX_CACHE_SIZE;
+      
+        // Ensure the cache size does not exceed the limit
+        if (messageCache.length > MAX_CACHE_SIZE) {
+          messageCache.shift();
+        }
+      }
+    });
 
-    // Check if the SimHash is similar to any cached SimHashes based on a threshold MAX_HAMMING_DISTANCE
-    const isSpamBySimHash = messageCache.some(
-      (cachedHash) => hammingDistance(simHash, cachedHash) <= config.MAX_HAMMING_DISTANCE
-    );
-    if (isSpamBySimHash) {
-      filteredIds.push(id); // Store the ID of the filtered post
-    } else {
-  // Update messageCache with the new SimHash
-  messageCache[cacheIndex] = simHash;
-  cacheIndex = (cacheIndex + 1) % MAX_CACHE_SIZE;
-  // Ensure the cache size does not exceed the limit
-  if (messageCache.length > MAX_CACHE_SIZE) {
-    messageCache.shift();
-  }
-}
-});
-
-// Send the filtered IDs back to the main script
-self.postMessage({ filteredIds });
-};
+    // Send the filtered IDs back to the main script
+    self.postMessage({ filteredIds });
+    };
