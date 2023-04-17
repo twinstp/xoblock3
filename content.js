be45573f9fe5c0433da0400ef8e8072fa00770fe
twinstp <twinstp@protonmail.com>
2023-04-17 19:12:03 -0400

    contentSelector

--- content.js
+++ content.js
@@ -162,7 +162,32 @@ chrome.storage.onChanged.addListener((changes, areaName) => {
     filterSpamPosts();
   }
 });
+  // Register a listener for the "addUserFilteredSubstring" message
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.type === 'addUserFilteredSubstring' && message.substring) {
+    addUserFilteredSubstring(message.substring);
+    sendResponse({ success: true });
+  }
+});
+
+if (chrome.storage) {
+  // Invoke the filterSpamPosts function again to apply changes if the configuration is updated
+  chrome.storage.onChanged.addListener((changes, areaName) => {
+    if (areaName === 'local' && changes.config) {
+      // Update the local configuration and filtered substrings
+      config = changes.config.newValue;
+      MAX_CACHE_SIZE = Math.max(config.MAX_CACHE_SIZE || 500, 1);
+      filteredSubstrings.clear();
+      config.FILTERED_SUBSTRINGS.forEach((substring) => filteredSubstrings.add(substring));
+      // Re-run the filter
+      filterSpamPosts();
+    }
+  });
   
+} else {
+  console.warn('chrome.storage is not available.');
+}
+
 })().catch((error) => {
   console.error('Error in extension:', error.message);
 });
