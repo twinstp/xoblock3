2fe530bf1478d4a7c46fa9abda75515f31160acd
twinstp <twinstp@protonmail.com>
2023-04-17 15:51:29 -0400

    mooo

--- options.js
+++ options.js
@@ -1,6 +1,8 @@
+// options.js
 document.addEventListener('DOMContentLoaded', () => {
     // Load the current configuration from chrome.storage.local and display it in the form fields
-    chrome.storage.local.get('config', ({ config }) => {
+    chrome.storage.local.get('config', (storedData) => {
+      const config = storedData.config;
       if (config) {
         document.getElementById('max-cache-size').value = config.MAX_CACHE_SIZE;
         document.getElementById('max-hamming-distance').value = config.MAX_HAMMING_DISTANCE;
@@ -13,12 +15,14 @@ document.addEventListener('DOMContentLoaded', () => {
       // Save the updated configuration to chrome.storage.local
       chrome.storage.local.set({ config }, () => {
         console.log('Configuration updated:', config);
+        // Notify the user that the configuration has been saved
+        alert('Configuration saved successfully.');
       });
     }
   
     document.getElementById('save-config').addEventListener('click', () => {
-      const maxCacheSize = parseInt(document.getElementById('max-cache-size').value);
-      const maxHammingDistance = parseInt(document.getElementById('max-hamming-distance').value);
+      const maxCacheSize = parseInt(document.getElementById('max-cache-size').value, 10);
+      const maxHammingDistance = parseInt(document.getElementById('max-hamming-distance').value, 10);
       const filteredSubstrings = document.getElementById('filtered-substrings').value.split(',');
   
       // Update configuration
@@ -27,9 +31,5 @@ document.addEventListener('DOMContentLoaded', () => {
         MAX_HAMMING_DISTANCE: maxHammingDistance,
         FILTERED_SUBSTRINGS: filteredSubstrings,
       });
-  
-      // Notify the user that the configuration has been saved
-      alert('Configuration saved successfully.');
     });
-  });
-  
+  });  
