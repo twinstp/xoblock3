51e3c5235f59631dd3e2a82f1d7c330a85b36d3e
twinstp <twinstp@protonmail.com>
2023-04-18 04:44:15 -0400

    pp

--- foreground.js
+++ foreground.js
@@ -17,17 +17,18 @@
     }
   });
 
-  // Invoke the filterSpamPosts function again to apply changes if the configuration is updated
-  if (chrome.storage) {
-    chrome.storage.onChanged.addListener((changes, areaName) => {
-      if (areaName === 'local' && changes.config) {
-        config.update(changes.config.newValue);
-        filterSpamPosts(config);
-      }
-    });
-  } else {
-    console.warn('chrome.storage is not available.');
-  }
+// Invoke the filterSpamPosts function again to apply changes if the configuration is updated
+if (chrome.storage) {
+  chrome.storage.onChanged.addListener((changes, areaName) => {
+    if (areaName === 'local' && changes.config) {
+      Object.assign(config, changes.config.newValue);
+      filterSpamPosts(config);
+    }
+  });
+} else {
+  console.warn('chrome.storage is not available.');
+}
+
 })().catch((error) => {
   console.error('Error in extension:', error.message);
 })();
