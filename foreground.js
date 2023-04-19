091a67a79bb4228b85a459db41b0ca3bf66343cf
twinstp <twinstp@protonmail.com>
2023-04-19 14:07:26 -0400

    yo

--- foreground.js
+++ foreground.js
@@ -1,34 +1,51 @@
-// foreground.js
-(async function () {
+(async function() {
   try {
     'use strict';
 
-    // Load the configuration and start filtering
+    // Log when the content script is loaded.
+    console.log('Content script loaded.');
+
+    // Attempt to load the configuration.
     const config = await loadConfig();
+    if (!config) {
+      console.error('Failed to load configuration.');
+      return;
+    }
+
+    // Log successful loading of configuration.
+    console.log('Configuration loaded:', config);
+
+    // Apply filtering.
     filterSpamPosts(config);
 
-  // Register a listener for the "addUserFilteredSubstring" message
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.type === 'addUserFilteredSubstring' && message.substring) {
-      addUserFilteredSubstring(message.substring, config);
-      sendResponse({ success: true });
-    }
-  });
+    // Add a listener for the "addUserFilteredSubstring" message.
+    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+      if (message.type === 'addUserFilteredSubstring' && message.substring) {
+        addUserFilteredSubstring(message.substring, config);
 
-// Invoke the filterSpamPosts function again to apply changes if the configuration is updated
-if (chrome.storage) {
-  chrome.storage.onChanged.addListener((changes, areaName) => {
-    if (areaName === 'local' && changes.config) {
-      Object.assign(config, changes.config.newValue);
-      filterSpamPosts(config);
-    }
-  });
-} else {
-  console.warn('chrome.storage is not available.');
-}
+        // Log the addition of a user-defined substring.
+        console.log(`Added user-defined substring "${message.substring}" to the filter list.`);
 
-} catch (error) {
-  // Handle any errors that occur in the extension
-  console.error('Error in extension:', error.message);
-}
+        sendResponse({ success: true });
+      }
+    });
+
+    // Add a listener for changes in the storage.
+    if (chrome.storage) {
+      chrome.storage.onChanged.addListener((changes, areaName) => {
+        if (areaName === 'local' && changes.config) {
+          Object.assign(config, changes.config.newValue);
+
+          // Log the update to the configuration.
+          console.log('Configuration updated:', config);
+
+          filterSpamPosts(config);
+        }
+      });
+    } else {
+      console.warn('chrome.storage is not available.');
+    }
+  } catch (error) {
+    console.error('Error in extension:', error.message);
+  }
 })();
