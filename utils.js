cee08c43f141267eb7ebb6bedc070f0716f94018
twinstp <twinstp@protonmail.com>
2023-04-22 10:43:27 -0400

    v2.3

--- utils.js
+++ utils.js
@@ -1,4 +1,17 @@
 'use strict';
+// Constants
+const LONG_POST_THRESHOLD = 300;
+const HAMMING_DISTANCE_THRESHOLD = 5;
+
+// Predefined spam substrings
+const nulloSubstrings = [
+  'modification, and he recently agreed to answer our questions',
+  'legal efforts to overturn the 2020 election; and three offenses relating to Trump’s unlawful possession of government records at Mar-a-Lago',
+  'America is in the midst of the Cold War. The masculine fire and fury of World War II has given way to a period of cooling',
+  'Go to the link, and look at that woman. Look at that face. She never expressed any remorse over',
+  'destroyed the Ancien Regime in Europe, was an economic and scientific golden era, but politically it was a mess.'
+];
+
 //SHA1 implementation (minified)
 var SHA1=
 "undefined"!=typeof exports?exports:{};!function(t){var r=[1518500249,1859775393,-1894007588,-899497514],i={sha1:1};SHA1.createHash=function(t){if(t&&!i[t]&&!i[t.toLowerCase()])throw new Error("Digest method not supported");return new s};var n,s=function(){function t(){this.A=1732584193,this.B=-271733879,this.C=-1732584194,this.D=271733878,this.E=-1009589776,this.t=0,this.i=0,(!n||e>=8e3)&&(n=new ArrayBuffer(8e3),e=0),this.h=new Uint8Array(n,e,80),this.o=new Int32Array(n,e,20),e+=80}return t.prototype.update=function(t){if("string"==typeof t)return this.u(t);if(null==t)throw new TypeError("Invalid type: "+typeof t);var r=t.byteOffset,i=t.byteLength,n=i/64|0,s=0;if(n&&!(3&r)&&!(this.t%64)){for(var h=new Int32Array(t.buffer,r,16*n);n--;)this.v(h,s>>2),s+=64;this.t+=s}if(1!==t.BYTES_PER_ELEMENT&&t.buffer){var e=new Uint8Array(t.buffer,r+s,i-s);return this.p(e)}return s===i?this:this.p(t,s)},t.prototype.p=function(t,r){var i=this.h,n=this.o,s=t.length;for(r|=0;r<s;){for(var h=this.t%64,e=h;r<s&&e<64;)i[e++]=t[r++];e>=64&&this.v(n),this.t+=e-h}return this},t.prototype.u=function(t){for(var r=this.h,i=this.o,n=t.length,s=this.i,h=0;h<n;){for(var e=this.t%64,f=e;h<n&&f<64;){var o=0|t.charCodeAt(h++);o<128?r[f++]=o:o<2048?(r[f++]=192|o>>>6,r[f++]=128|63&o):o<55296||o>57343?(r[f++]=224|o>>>12,r[f++]=128|o>>>6&63,r[f++]=128|63&o):s?(o=((1023&s)<<10)+(1023&o)+65536,r[f++]=240|o>>>18,r[f++]=128|o>>>12&63,r[f++]=128|o>>>6&63,r[f++]=128|63&o,s=0):s=o}f>=64&&(this.v(i),i[0]=i[16]),this.t+=f-e}return this.i=s,this},t.prototype.v=function(t,i){var n=this,s=n.A,e=n.B,f=n.C,w=n.D,y=n.E,A=0;for(i|=0;A<16;)h[A++]=o(t[i++]);for(A=16;A<80;A++)h[A]=u(h[A-3]^h[A-8]^h[A-14]^h[A-16]);for(A=0;A<80;A++){var p=A/20|0,d=a(s)+v(p,e,f,w)+y+h[A]+r[p]|0;y=w,w=f,f=c(e),e=s,s=d}this.A=s+this.A|0,this.B=e+this.B|0,this.C=f+this.C|0,this.D=w+this.D|0,this.E=y+this.E|0},t.prototype.digest=function(t){var r=this.h,i=this.o,n=this.t%64|0;for(r[n++]=128;3&n;)r[n++]=0;if((n>>=2)>14){for(;n<16;)i[n++]=0;n=0,this.v(i)}for(;n<16;)i[n++]=0;var s=8*this.t,h=(4294967295&s)>>>0,e=(s-h)/4294967296;return e&&(i[14]=o(e)),h&&(i[15]=o(h)),this.v(i),"hex"===t?this.I():this.U()},t.prototype.I=function(){var t=this,r=t.A,i=t.B,n=t.C,s=t.D,h=t.E;return f(r)+f(i)+f(n)+f(s)+f(h)},t.prototype.U=function(){var t=this,r=t.A,i=t.B,n=t.C,s=t.D,h=t.E,e=t.h,f=t.o;return f[0]=o(r),f[1]=o(i),f[2]=o(n),f[3]=o(s),f[4]=o(h),e.slice(0,20)},t}(),h=new Int32Array(80),e=0,f=function(t){return(t+4294967296).toString(16).substr(-8)},o=254===new Uint8Array(new Uint16Array([65279]).buffer)[0]?function(t){return t}:function(t){return t<<24&4278190080|t<<8&16711680|t>>8&65280|t>>24&255},u=function(t){return t<<1|t>>>31},a=function(t){return t<<5|t>>>27},c=function(t){return t<<30|t>>>2};function v(t,r,i,n){return 0===t?r&i|~r&n:2===t?r&i|r&n|i&n:r^i^n}}();
@@ -236,7 +249,17 @@ class TrieNode {
     return current.isEndOfWord;
   }
 }
+// Initialize Bloom filter, LRU cache, Trie node, and SimHash generator
+const bloomFilter = new BloomFilter(10000, 5);
+const lruCache = new LRUCache(1000);
+const substringTrie = new TrieNode();
+const simHashGenerator = new SimHashGenerator();
 
+// Populate Trie node and Bloom filter with predefined substrings
+nulloSubstrings.forEach(substring => {
+  substringTrie.insert(substring);
+  bloomFilter.add(substring);
+});
 function escapeRegexSpecialCharacters(str) {
   return str.replace(/[-[\]\/{}()*+?.,\\^$|#\s]/g, '\\$&');
 }
@@ -371,49 +394,83 @@ class SimHashGenerator {
     }
   }
   
-  // Define a function to hide an element by ID
-  function hideElementById(id) {
-    const element = document.getElementById(id);
-    if (element) {
-      element.style.display = 'none';
+// Define a function to hide an element by ID
+function hideElementById(id) {
+  const element = document.getElementById(id);
+  if (element) {
+    element.style.display = 'none';
+  }
+}
+
+// Define a function to hide elements by selector
+function hideElementBySelector(selector) {
+  const elements = document.querySelectorAll(selector);
+  elements.forEach((element) => {
+    element.style.display = 'none';
+  });
+}
+
+// Define a function to check if a post is spam
+function shouldHidePost(content, author, config) {
+  return config.FILTERED_SUBSTRINGS.some(substring => content.includes(substring));
+}
+
+// Handle errors
+function catchErrors() {
+  window.addEventListener('error', (error) => {
+    console.error('Error in extension:', error.message);
+  });
+}
+
+// Main function to filter spam posts
+async function filterSpamPosts(config) {
+  // Get post elements
+  const messageTables = document.querySelectorAll("table[width='700']");
+  
+  const computePromises = messageTables.map(async (table) => {
+    const content = extractPostText(table);
+    const id = table.id || table.getAttribute('name');
+
+    if (content.length < LONG_POST_THRESHOLD) {
+      return;
     }
-  }
-  
-  // Define a function to hide elements by selector
-  function hideElementBySelector(selector) {
-    const elements = document.querySelectorAll(selector);
-    elements.forEach((element) => {
-      element.style.display = 'none';
+
+    if (shouldHidePost(content, null, config)) {
+      hideElementById(id);
+      return;
+    }
+
+    const simHash = simHashGenerator.compute(content);
+    if (bloomFilter.test(simHash) || lruCache.get(simHash)) {
+      hideElementById(id);
+      return;
+    }
+
+    let isSpam = false;
+    lruCache.forEach((value, cachedSimHash) => {
+      if (SimHashGenerator.hammingDistance(simHash, cachedSimHash) <= HAMMING_DISTANCE_THRESHOLD) {
+        isSpam = true;
+        return false; // Break out of the loop
+      }
     });
-  }
-  
-  // Define a function to check if a post is spam
-  function shouldHidePost(content, author, config) {
-    return config.FILTERED_SUBSTRINGS.some(substring => content.includes(substring));
-  }
-  
-  // Handle errors.
-  function catchErrors() {
-    window.addEventListener('error', (error) => {
-      console.error('Error in extension:', error.message);
-    });
-  }
-  
-  // Main function to initialize the extension and run tests.
-  (async () => {
-    const config = await loadConfig();
-    registerAddUserFilteredSubstringListener(config);
-    registerConfigChangeListener(config, SimHashGenerator.filterSpamPosts);
-    catchErrors();
-    await SimHashGenerator.filterSpamPosts(config);
-  })();  
-  (async () => {
-    const config = await loadConfig();
-    registerAddUserFilteredSubstringListener(config);
-    registerConfigChangeListener(config, SimHashGenerator.filterSpamPosts);
-    catchErrors();
-    await SimHashGenerator.filterSpamPosts(config);
-  })();
+
+    lruCache.put(simHash, isSpam);
+    if (isSpam) {
+      hideElementById(id);
+    }
+  });
+
+  await Promise.all(computePromises);
+}
+
+// Main function to initialize the extension and run tests
+(async () => {
+  const config = await loadConfig();
+  registerAddUserFilteredSubstringListener(config);
+  registerConfigChangeListener(config, filterSpamPosts);
+  catchErrors();
+  await filterSpamPosts(config);
+})();
   
   function testHammingDistance() {
     const hash1 = 0b11010101n;
