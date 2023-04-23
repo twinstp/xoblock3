2ad79128f1b6e4f2cbddd9d2c63ff0061cb0ad15
twinstp <twinstp@protonmail.com>
2023-04-23 14:38:26 -0400

    selector2

--- content.js
+++ content.js
@@ -327,21 +327,63 @@ async function loadConfig() {
   });
 }
 
+function isAllWhitespace(text) {
+  return /^\s*$/.test(text);
+}
+
+function extractText(input) {
+  const regex = /\(http:\/\/www\.autoadmit\.com\/thread\.php\?thread_id=\d+&forum_id=\d+#\d+\)$/;
+  if (regex.test(input)) {
+    return input.replace(regex, '').trim();
+  }
+  if (input.startsWith(')')) {
+    input = input.substring(1);
+  }
+  return input;
+}
+
 function getPostElements() {
-  const postElements = document.querySelectorAll("body > a > table > tbody > tr > td > font > p");
-  const posts = Array.from(postElements).map((postElement) => {
-    // Extract the relevant details from the postElement here.
-    const dateElement = postElement.querySelector("b");
-    const dateStr = dateElement ? dateElement.nextSibling.textContent.trim() : null;
-    const authorElement = postElement.querySelector("b + b");
-    const author = authorElement ? authorElement.nextSibling.textContent.trim() : null;
-    const contentElement = authorElement ? authorElement.nextElementSibling : null;
-    const content = contentElement ? contentElement.textContent.trim() : null;
-    const id = postElement.closest('table').id; // Get the table's id attribute
-    return { date: dateStr, author, content, id };
-  }).filter(post => post.author && post.content);
-  console.log('Found post elements:', posts);
-  return posts;
+  const messageTables = document.querySelectorAll("table[width='700']");
+  const posts = [];
+  for (const table of messageTables) {
+    const cellspacing = table.getAttribute('cellspacing') ? 'cellspacing' : null;
+    if (cellspacing) {
+      continue;
+    }
+    const authorElement = Array.from(table.querySelectorAll('b')).find((b) => b.textContent.trim() === 'Author:');
+    const author = authorElement ? authorElement.nextSibling?.textContent?.trim() : null;
+    const dateElement = Array.from(table.querySelectorAll('b')).find((b) => b.textContent.trim() === 'Date:');
+    const dateStr = dateElement ? dateElement.nextSibling?.textContent?.trim() : null;
+    const bodyElement = table.querySelector('table font');
+    let i = 0;
+    let authorDetected = false;
+    const bodyStrings = [];
+    if (!bodyElement) {
+      continue;
+    }
+    for (const child of bodyElement.childNodes) {
+      if (i < 2) {
+        i++;
+        continue;
+      }
+      if (child.textContent && !isAllWhitespace(child.textContent)) {
+        const textContent = child.textContent;
+        if (!authorDetected) {
+          if (textContent.startsWith('Author:')) {
+            authorDetected = true;
+            i = 0;
+          }
+          continue;
+        }
+        bodyStrings.push(extractText(child.textContent));
+      }
+      i++;
+    }
+    const content = bodyStrings.join('');
+    const id = table.id;
+    posts.push({ date: dateStr, author, content, id });
+  }
+  return posts.filter(post => post.author && post.content);
 }
 // Hide an HTML element by its ID.
 function hideElementById(id) {
